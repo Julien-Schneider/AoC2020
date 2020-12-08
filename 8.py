a3b52afffb8327a9bcf7fb51b7f65a9cde075637
Julien Schneider <julien.schneider@tu-ilmenau.de>
2020-12-08 09:03:26 +0100

    Added results

--- 8.py
+++ 8.py
@@ -7,10 +7,12 @@ print((fun:= lambda i, acc, visited: acc if i in visited else fun(*{
         "acc": lambda i, acc, val: (i+1, acc + int(val)),
         "jmp": lambda i, acc, val: (i+int(val), acc)
         }[data[i][:3]](i, acc, data[i][3:]), visited | {i}))(0, 0, set()))
+# 1928
 
 print(max([(fun:= lambda i, acc, visited, inst: (i, acc) if i in visited else fun(*{
         "nop": lambda i, acc, val: (i+1, acc),
         "acc": lambda i, acc, val: (i+1, acc + int(val)),
         "jmp": lambda i, acc, val: (i+int(val), acc),
         "": lambda i, acc, val: (i, acc)
-        }[inst[i][:3]](i, acc, inst[i][3:]), visited | {i}, inst))(0, 0, set(), data[:i] + [["nop", "jmp"][line[:3]=="nop"] + line[3:]] + data[i+1:]) for i, line in enumerate(data[:-1])]))
+        }[inst[i][:3]](i, acc, inst[i][3:]), visited | {i}, inst))(0, 0, set(), data[:i] + [["nop", "jmp"][line[:3]=="nop"] + line[3:]] + data[i+1:]) for i, line in enumerate(data[:-1])])[1])
+# 1319
