8cdccb89711a1463a0d74489f9ff7c375db73159
Julien Schneider <julien.schneider@tu-ilmenau.de>
2020-12-14 11:58:21 +0100

    Adjusted

--- 10.py
+++ 10.py
@@ -1,10 +1,14 @@
 import helper
 from functools import lru_cache
+from math import prod
 
 data = [0] + sorted([int(x) for x in helper.get_input(10).split("\n")[:-1]])
 
 print((t:=[y - x for x, y in zip(data, data[1:] + [max(data) + 3])]).count(1) * t.count(3))
 # 2400
+
+print(prod([[1, 1, 2, 4, 7][len(x)] for x in "".join(map(str, t)).split("3")]))
+# 338510590509056
 
 print((fun:= lru_cache(lambda i: 1 if data[i] == max(data) else sum([fun(i+j+1) * ((data[i+j+1] - data[i]) <= 3) for j, _ in enumerate(data[i+1:i+4])])))(0))
 # 338510590509056
--- 11.py
+++ 11.py
@@ -3,7 +3,6 @@ from itertools import product, chain
 
 data = [[{"L": 0, ".": None}[x] for x in line] for line in helper.get_input(11).split("\n")[:-1]]
 
-
 print(list(chain(*(fun:= lambda seats: new_seats if (new_seats:= [[{None: lambda c: None, 1: lambda c: c < 4, 0: lambda c: c == 0}[seats[y][x]]([seats[y+dy][x+dx] for dx, dy in product((0, 1, -1), (0, 1, -1)) if 0 <= y+dy < len(seats) and 0 <= x+dx < len(seats[0])][1:].count(1)) for x in range(len(seats[0]))] for y in range(len(seats))]) == seats else fun(new_seats))(data))).count(1))
 # 2481
 
